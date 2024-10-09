9d0412832b5b34f617a4ea9708b564051afd033b
Yemin-YAYA <yaya.yemin@laposte.net>
2024-10-09 08:50:55 +0200

    Modification css bar de nav

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Post;
 use App\Entity\Rubrik;
 use App\Entity\Comment;
@@ -38,74 +39,73 @@ class PostController extends AbstractController
             'posts' => $posts,
         ]);
     }
- 
-      //GESTION DE LA RECUPERATION DU DETAIL D'UN POST(TOTALITE D'UN POST)+Commentaires
-    
-      #[Route('/post/{id}/{slug}', name: 'show', requirements: ['id' => '\d+'])]
-      public function showone(Post $post, Request $req, CommentRepository $crepo): Response
-      {
-          $comment = new Comment();
-          $commentForm = $this->createForm(CommentFormType::class, $comment);
-          $commentForm->handleRequest($req);
-  
-          // Soumettre le commentaire via le formulaire
-          if ($commentForm->isSubmitted()) {
-              if ($commentForm->isValid()) {
-                  // Vérifier si l'utilisateur est connecté
-                  if (!$this->getUser()) {
-                      $this->addFlash('warning', 'Vous devez être connecté pour laisser un commentaire.');
-                      return $this->redirectToRoute('app_login');
-                  }
-  
-                  // Persister les données du commentaire
-                  $comment->setUser($this->getUser());
-                  $comment->setPost($post);
-                  $comment->setCreatedAt(new \DateTimeImmutable());
-  
-                  try {
-                      $this->emi->persist($comment);
-                      $this->emi->flush();
-  
-                      // Rediriger pour éviter la resoumission du formulaire
-                      return $this->redirectToRoute('show', [
-                          'id' => $post->getId(),
-                          'slug' => $post->getSlug()
-                      ]);
-                  } catch (\Exception $e) {
-                      $this->addFlash('danger', 'Erreur lors de l\'envoi du commentaire.');
-                      dump($e->getMessage()); // Pour debug
-                  }
-              } else {
-                  // Affichage des erreurs en cas d'invalidité
-                  dump($commentForm->getErrors(true));
-              }
-          }
-  
-          // Récupération des commentaires pour affichage
-          $allComments = $crepo->findByPostOrderedByCreatedAtDesc($post->getId());
-  
-          return $this->render('show/show.html.twig', [
-              'post' => $post,
-              'comments' => $allComments,
-              'comment_form' => $commentForm->createView(),
-          ]);
-      }
+
+    //GESTION DE LA RECUPERATION DU DETAIL D'UN POST(TOTALITE D'UN POST)+Commentaires
+
+    #[Route('/post/{id}/{slug}', name: 'show', requirements: ['id' => '\d+'])]
+    public function showone(Post $post, Request $req, CommentRepository $crepo): Response
+    {
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentFormType::class, $comment);
+        $commentForm->handleRequest($req);
+
+        // Soumettre le commentaire via le formulaire
+        if ($commentForm->isSubmitted()) {
+            if ($commentForm->isValid()) {
+                // Vérifier si l'utilisateur est connecté
+                if (!$this->getUser()) {
+                    $this->addFlash('warning', 'Vous devez être connecté pour laisser un commentaire.');
+                    return $this->redirectToRoute('app_login');
+                }
+
+                // Persister les données du commentaire
+                $comment->setUser($this->getUser());
+                $comment->setPost($post);
+                $comment->setCreatedAt(new \DateTimeImmutable());
+
+                try {
+                    $this->emi->persist($comment);
+                    $this->emi->flush();
+
+                    // Rediriger pour éviter la resoumission du formulaire
+                    return $this->redirectToRoute('show', [
+                        'id' => $post->getId(),
+                        'slug' => $post->getSlug()
+                    ]);
+                } catch (\Exception $e) {
+                    $this->addFlash('danger', 'Erreur lors de l\'envoi du commentaire.');
+                    dump($e->getMessage()); // Pour debug
+                }
+            } else {
+                // Affichage des erreurs en cas d'invalidité
+                dump($commentForm->getErrors(true));
+            }
+        }
+
+        // Récupération des commentaires pour affichage
+        $allComments = $crepo->findByPostOrderedByCreatedAtDesc($post->getId());
+
+        return $this->render('show/show.html.twig', [
+            'post' => $post,
+            'comments' => $allComments,
+            'comment_form' => $commentForm->createView(),
+        ]);
+    }
     //Gestion de l'affichage des rubriques
-    #[Route('/rubrik/{id}', name:'posts_by_rubrik')]
-    public function postsByRubrik(Rubrik $rubrik, $id, PostRepository $postRepository, 
-    RubrikRepository $rubrikRepository): Response
+    #[Route('/rubrik/{id}', name: 'posts_by_rubrik')]
+    public function postsByRubrik(Rubrik $rubrik, $id, PostRepository $postRepository, RubrikRepository $rubrikRepository): Response
     {
         $rubrik = $rubrikRepository->find($id);
         if (!$rubrik) {
             throw $this->createNotFoundException('rubrik not found');
         }
-    
+
         $posts = $postRepository->findByRubrik($rubrik);
-        
-       
-            return $this->render('rubrik/rubrik.html.twig', [
-                'rubrik' => $rubrik,
-                'posts' => $posts
-            ]);
-        }
+
+
+        return $this->render('rubrik/rubrik.html.twig', [
+            'rubrik' => $rubrik,
+            'posts' => $posts
+        ]);
+    }
 }
